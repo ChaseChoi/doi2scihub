c8fb2059213b5c7aa1fcc0eb025108700a824308
ChaseChoi <caiguihao@gmail.com>
2020-10-06 14:04:18 +0800

    Improve metadata indentation

--- doi2scihub.user.js
+++ doi2scihub.user.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name         		DOI to Sci-Hub
-// @name:zh-CN	 		DOI跳转Sci-Hub
-// @namespace    		https://greasyfork.org/users/692574
-// @version      		1.0.0
-// @description  		Highlight DOI link on the current webpage and redirect it to Sci-Hub.
-// @description:zh-CN	高亮当前页面的DOI链接，并重定向至Sci-Hub。
-// @author       		Chase Choi
-// @license				MIT
-// @match        		https://www.sciencedirect.com/*
-// @match        		https://onlinelibrary.wiley.com/doi/*
-// @match 	     		https://academic.oup.com/*
-// @match 		 		https://journals.sagepub.com/*
-// @match 		 		https://link.springer.com/article/*
-// @require      		https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
-// @grant        		GM.xmlHttpRequest
+// @name               DOI to Sci-Hub
+// @name:zh-CN         DOI跳转Sci-Hub
+// @namespace          https://greasyfork.org/users/692574
+// @version            1.0.0
+// @description        Highlight DOI link on the current webpage and redirect it to Sci-Hub.
+// @description:zh-CN  高亮当前页面的DOI链接，并重定向至Sci-Hub。
+// @author             Chase Choi
+// @license            MIT
+// @match              https://www.sciencedirect.com/*
+// @match              https://onlinelibrary.wiley.com/doi/*
+// @match              https://academic.oup.com/*
+// @match              https://journals.sagepub.com/*
+// @match              https://link.springer.com/article/*
+// @require            https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @grant              GM.xmlHttpRequest
 // ==/UserScript==
 
 (function () {
