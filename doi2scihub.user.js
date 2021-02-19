ff5338201ddd1aaa835ac71870f44b33ba8a56f9
ChaseChoi <caiguihao@gmail.com>
2021-02-19 15:01:02 +0800

    Bug fix for CNKI Scholar

--- doi2scihub.user.js
+++ doi2scihub.user.js
@@ -2,7 +2,7 @@
 // @name                DOI to Sci-Hub
 // @name:zh-CN          DOI跳转Sci-Hub
 // @namespace           https://greasyfork.org/users/692574
-// @version             1.0.13
+// @version             1.0.14
 // @description         Highlight DOI link on the current webpage and redirect it to Sci-Hub.
 // @description:zh-CN   高亮当前页面的DOI链接，并重定向至Sci-Hub。
 // @author              Chase Choi
@@ -24,6 +24,7 @@
 // @match               https://*.sciencemag.org/*
 // @match               https://pubs.rsc.org/*
 // @match               https://schlr.cnki.net/Detail/index/*
+// @match               https://schlr.cnki.net//Detail/index/*
 // @require             https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @grant               GM.xmlHttpRequest
 // ==/UserScript==
