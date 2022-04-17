2ff1d259503560b66dded062139d9b1fa808eb6b
ChaseChoi <caiguihao@gmail.com>
2022-04-17 21:08:03 +0800

    Update metadata

--- doi2scihub.user.js
+++ doi2scihub.user.js
@@ -11,7 +11,6 @@
 // @match               http*://*.webofknowledge.com/*
 // @match               https://academic.oup.com/*
 // @match               https://academic.microsoft.com/*
-// @require             https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @match               https://ieeexplore.ieee.org/*
 // @match               https://journals.sagepub.com/*
 // @match               https://link.springer.com/*
@@ -34,6 +33,7 @@
 // @match               https://www.tandfonline.com/*
 // @match               https://www.thieme-connect.com/products/ejournals/*
 // @match               https://www.webofscience.com/wos/*
+// @require             https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // @grant               GM.xmlHttpRequest
 // ==/UserScript==
 
